478a7e9b068ab31f388900d578b3f9063903de6b
Anderson Martins <amoraesmartins@gmail.com>
2024-05-06 15:54:27 -0300

    Desafios DIO entregues

---
+++ Formação Python Developer/desafio_aproveite_a_oferta.py
@@ -0,0 +1,24 @@
+'''Desafio
+Um supermercado está fazendo uma promoção de venda de refrigerantes. Se um dia você comprar refrigerantes e levar os cascos vazios no dia seguinte, ela troca cada conjunto de K garrafas vazias  por uma garrafa cheia. Um cliente quer aproveitar ao máximo essa oferta e por isso comprou várias garrafas no primeiro dia da promoção. Agora ele quer saber quantas garrafas terá ao final do segundo dia da promoção, se usá-la ao máximo.
+
+Faça um programa para calcular isso.
+
+Entrada
+A primeira linha de entrada contém inteiro T (1 ≤ T ≤ 10000) , que indica o número de casos de teste. Em cada uma das T linhas a seguir vêm dois inteiros N e K (1 ≤ K, N ≤ 10000),  respectivamente o número de refrigerantes comprados e o número de garrafas vazias para ganhar uma cheia.
+
+Saída
+Para cada caso de teste imprima o número de garrafas que o cliente terá no segundo dia, se aproveitar ao máximo a oferta.
+'''
+T = int(input()) #Lendo o número de casos de teste
+
+while T > 0: #Loop while que executa T vezes
+  T -= 1 #Decrementa T a cada iteração do loop
+  N, K = input().split()
+  N = int(N)
+  K = int(K)
+  '''Divide o número de garrafas vazias pelo número de cascos necessários para trocar por uma garrafa cheia
+     Calcula o resto da divisão de N por K para contar as garrafas que não podem ser trocadas por uma cheia
+     Soma o resultado das duas operações para obter o total'''
+  total = int(int(N % K) + int(N /K))
+    
+  print(total)
---
+++ Formação Python Developer/desafio_dragao.py
@@ -0,0 +1,16 @@
+'''Desafio
+Daenerys é a khaleesi dos Dothraki. Juntamente com Drogon, eles vão atrás do Tyrion, para tentar dominar Westeros. Ela possui, além do seu dragãozinho, um rastreador que mede o nível de energia de qualquer ser vivo. Todos os seres com o nível menor ou igual a 8000, ela considera como se fosse um inseto. Quando passa deste valor, que foi o caso do Drogon, ela se espanta e grita “Mais de 8000”. Baseado nisso, utilize a mesma tecnologia e analise o nível de energia dos seres vivos.
+
+Entrada
+A primeira linha contém um número inteiro C relativo ao número de casos de teste. Em seguida, haverá C linhas, com um número inteiro N (100 <= N <= 100000) relativo ao nível de energia de um ser vivo.
+
+Saída
+Para cada valor lido, imprima o texto correspondente.
+'''
+C = int(input())  # Lendo o número de casos de teste
+for i in range(C):  # Iterando sobre cada caso de teste
+    nivel_energia = int(input())  # Lendo o nível de energia do ser vivo
+    if nivel_energia <= 8000:
+        print("Inseto!")
+    else:
+        print("Mais de 8000")
