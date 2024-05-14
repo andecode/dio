417ff6f149783a1a45ba15077d8dfb5c4cc47896
Anderson Martins <157650633+andecode@users.noreply.github.com>
2024-05-14 09:13:36 -0300

    descrição desafios

--- python-data-Analytics/desafio-jornada-classificacao-frutifera.py
+++ python-data-Analytics/desafio-jornada-classificacao-frutifera.py
@@ -1,3 +1,37 @@
+'''Descrição
+Nesta missão, sua tarefa é mais desafiadora do que nunca! Em um pomar mágico, as frutas têm características 
+únicas que as diferenciam. Seu objetivo é criar um modelo de machine learning capaz de classificar 
+frutas com base em três características: peso, textura (suave ou áspera) e cor (vermelha, laranja 
+ou amarela). Cada tipo de fruta tem limites específicos para essas características.
+
+Morango:
+Peso mínimo: 150 gramas
+Textura: Suave (smooth)
+Cor: Vermelha (red)
+Maçã:
+Peso mínimo: 130 gramas
+Textura: Ápera (rough)
+Cor: Vermelha (red)
+Laranja:
+Peso mínimo: 120 gramas
+Textura: Suave (smooth)
+Cor: Laranja (orange)
+Banana:
+Peso mínimo: 150 gramas
+Textura: Áspera (rough)
+Cor: Amarela (yellow)
+Entrada
+Seu código deve receber as seguintes entradas do usuário:
+
+Peso da fruta (em gramas): um número real que representa o peso da fruta.
+Textura da fruta (suave ou áspera): uma string indicando se a fruta é suave ("smooth") ou 
+áspera ("rough").
+Cor da fruta (vermelha, laranja ou amarela): uma string indicando a cor da fruta.
+Saída
+O código deve produzir uma saída indicando a classificação da fruta com base nas características 
+fornecidas.
+'''
+
 def prever_fruta(peso, textura, cor):
     # Lógica de decisão baseada nas características fornecidas
     if peso >= 150 and textura == "smooth" and cor == "red":
